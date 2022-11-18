f3c0949ced26377da28ea5f3f032375287b1d0ad
AlexKouremenos <kourekouremenos@gmail.com>
2022-11-19 00:15:28 +0200

    Init Aurantium submodule.

---
+++ aurantium/src/main/java/com/rue/aurantium/AurantiumApplication.java
@@ -0,0 +1,13 @@
+package com.rue.aurantium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurantiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AurantiumApplication.class, args);
+    }
+
+}
---
+++ aurantium/src/test/java/com/rue/aurantium/AurantiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rue.aurantium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurantiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
